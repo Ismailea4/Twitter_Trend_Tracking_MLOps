83e04458bb2df98612cd52a653e74600d9b96900
Ismail ELADRAOUI <eladraoui.ismail4@gmail.com>
2025-04-15 11:57:10 +0100

    Testing 6 github action

--- src/preprocess.py
+++ src/preprocess.py
@@ -34,6 +34,10 @@ def preprocess_data(base_dir="../data_scraped/",
 
     for company in companies:
         company_path = os.path.join(base_dir, company)
+        if not os.path.exists(company_path):
+            print(f"Directory does not exist: {company_path}")
+            continue  # Skip missing directories
+        
         for date_folder in tqdm(os.listdir(company_path), desc=f"Processing {company}"):
             date_path = os.path.join(company_path, date_folder)
             for csv_file in os.listdir(date_path):
