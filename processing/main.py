9df2b186adffe9f6aa78193aee6de44f7a46a1e8
Ismail ELADRAOUI <eladraoui.ismail4@gmail.com>
2025-05-29 11:58:40 +0100

    Adding segmentation and engagement forecasting models

--- processing/main.py
+++ processing/main.py
@@ -1,13 +1,11 @@
-from src.preprocess import remove_empty_files, preprocess_data
 import os
+import sys
+from src.preprocess import remove_empty_files, preprocess_data
 
 
 if __name__ == "__main__":
-    SCRIPT_DIR = os.path.dirname(os.path.abspath(__file__))
-    DATA_PATH = os.path.join(SCRIPT_DIR, "..", "data_scraped")
-    
-    remove_empty_files(DATA_PATH, ['samsung', 'apple', 'nintendo'])
-    df_tweets, df_users = preprocess_data()
-    df_tweets.to_csv("cleaned_tweet_data.csv", index=False)
-    df_users.to_csv("cleaned_user_data.csv", index=False)
+    remove_empty_files("data_scraped/", ['samsung', 'apple', 'nintendo'])
+    df_tweets, df_users = preprocess_data("data_scraped/", ['samsung', 'apple', 'nintendo'])
+    df_tweets.to_csv("processing/cleaned_tweet_data.csv", index=False)
+    df_users.to_csv("processing/cleaned_user_data.csv", index=False)
     print("Preprocessing complete.")
